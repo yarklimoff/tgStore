ad674728d9465883249502841cc8b9971daa1988
Klimov Yaroslav <klimov.iav@phystech.edu>
2024-10-15 01:43:29 +0300

    ашч ыскщдд

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,50 @@ import { Catalog } from './pages/catalog/catalog';
 import { Item } from './pages/item/item';
 import 'normalize.css';
 import cn from './App.module.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom';
 import { Cart } from './pages/cart/cart';
 import { Global, GlobalContext } from './global';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Catalog />,
+    element: (
+      <>
+        <ScrollToTop />
+        <Catalog />
+      </>
+    ),
   },
   {
     path: '/catalog/:article',
-    element: <Item />,
+    element: (
+      <>
+        <ScrollToTop />
+        <Item />
+      </>
+    ),
   },
   {
     path: '/cart',
-    element: <Cart />,
+    element: (
+      <>
+        <ScrollToTop />
+        <Cart />
+      </>
+    ),
   },
 ]);
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Сбрасываем скролл на верх при каждом изменении маршрута
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const global = useMemo(() => new Global(), []);
